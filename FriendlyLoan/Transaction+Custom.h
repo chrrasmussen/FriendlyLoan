b306e025242df70a67581dbe4ebddc9541855662
Christian Rasmussen <christian.rasmussen@me.com>
2011-09-10 02:51:40 +0200

    Changed name of Transaction category.

---
+++ FriendlyLoan/Transaction+Custom.h
@@ -0,0 +1,15 @@
+//
+//  Transaction+CustomMethods.h
+//  FriendlyLoan
+//
+//  Created by Christian Rasmussen on 10.09.11.
+//  Copyright (c) 2011 Rasmussen I/O. All rights reserved.
+//
+
+#import "Transaction.h"
+
+@interface Transaction (Custom)
+
+- (NSString *)historySectionName;
+
+@end
