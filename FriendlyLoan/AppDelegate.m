a5d88407c8d846e94616abbcab3afd5ed490952c
Christian Rasmussen <christian.rasmussen@me.com>
2012-02-06 23:19:08 +0100

    Added code for CoreData-migration.

--- FriendlyLoan/AppDelegate.m
+++ FriendlyLoan/AppDelegate.m
@@ -37,7 +37,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    NSLog(@"Init");
+    NSLog(@"application:didFinishLaunchingWithOptions:");
+    
     [self setUpTimedLocationManager];
     
     self.transactionsAwaitingLocation = [[NSMutableSet alloc] init];
@@ -135,11 +136,15 @@
         return __persistentStoreCoordinator;
     }
     
+    
     NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"FriendlyLoan.sqlite"];
+    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+                             [NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption,
+                             [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
     
     NSError *error = nil;
     __persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-    if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error])
+    if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error])
     {
         /*
          Replace this implementation with code to handle the error appropriately.
